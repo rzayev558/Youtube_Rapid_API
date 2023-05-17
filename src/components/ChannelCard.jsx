c02b2a980ca66c8b4040f568391350da810a02cb
rzayev558 <rzayev558@gmail.com>
2023-05-17 16:01:45 +0200

    Fetch videos on profile

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,7 +3,7 @@ import { Box, CardContent, CardMedia, Typography } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
-function ChannelCard({ channelDetail }) {
+function ChannelCard({ channelDetail, marginTop }) {
   return (
     <Box
       sx={{
@@ -15,6 +15,7 @@ function ChannelCard({ channelDetail }) {
         width: { xs: "356px", md: "320px" },
         height: "326px",
         margin: "auto",
+        marginTop,
       }}
     >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
